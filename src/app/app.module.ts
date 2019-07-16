ace15522612838f16290bf3a212ebd64097610e4
Hardik Joshi <joshi.hardik654@gmail.com>
2019-07-16 23:07:27 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-list/recipe-detail/recipe-detail.component';
+import { ShoppingItemComponent } from './shopping-list/shopping-item/shopping-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Pav Bhaji','Typical mumbai dish', 'https://hebbarskitchen.com/wp-content/uploads/mainPhotos/pav-bhaji-recipe-easy-mumbai-style-pav-bhaji-recipe-1-1068x1423.jpeg'),
+    new Recipe('Mysore Masala Dosa', 'Its a South Indian dish specially famous in mysore. It is very spicy dish with very rich taste.', 'https://hebbarskitchen.com/wp-content/uploads/mainPhotos/mysore-masala-dosa-recipe-mysore-dosa-mysore-masala-dose-26-1068x1423.jpeg'),
+    new Recipe('Pav Bhaji','Typical mumbai dish', 'https://hebbarskitchen.com/wp-content/uploads/mainPhotos/pav-bhaji-recipe-easy-mumbai-style-pav-bhaji-recipe-1-1068x1423.jpeg'),
+    new Recipe('Mysore Masala Dosa', 'Its a South Indian dish specially famous in mysore. It is very spicy dish with very rich taste.', 'https://hebbarskitchen.com/wp-content/uploads/mainPhotos/mysore-masala-dosa-recipe-mysore-dosa-mysore-masala-dose-26-1068x1423.jpeg'),
+    new Recipe('Pav Bhaji','Typical mumbai dish', 'https://hebbarskitchen.com/wp-content/uploads/mainPhotos/pav-bhaji-recipe-easy-mumbai-style-pav-bhaji-recipe-1-1068x1423.jpeg'),
+    new Recipe('Mysore Masala Dosa', 'Its a South Indian dish specially famous in mysore. It is very spicy dish with very rich taste.', 'https://hebbarskitchen.com/wp-content/uploads/mainPhotos/mysore-masala-dosa-recipe-mysore-dosa-mysore-masala-dose-26-1068x1423.jpeg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
