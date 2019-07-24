b945221e70a0086fc1382807012a426a66a524f2
hardik-joshi <joshi.hardik654@gmail.com>
2019-07-24 02:17:47 -0400

    Using angular directive now we can toggle dropdowns

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, HostListener, HostBinding, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.show') isOpen = false;
+  private dropdownParentEl = this.elementRef.nativeElement.closest('.dropdown');
+
+  constructor(private elementRef: ElementRef) {}
+
+  @HostListener('click') toggleClick() {
+    this.dropdownParentEl.querySelector('.dropdown-menu').classList.toggle('show');
+  }
+}
