712918766763bd3ee305bebbac1c9714abbf3f44
Hardik Joshi <joshi.hardik654@gmail.com>
2019-07-28 23:11:32 +0530

    added recipe's ingredient in shopping list with the help for services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
@@ -59,7 +61,13 @@ export class RecipeService {
             ]),
     ];
 
+    constructor(private shoppingListService: ShoppingListService) {}
+
     getRecipe() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
     }
 }
