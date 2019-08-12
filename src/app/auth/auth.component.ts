06b57e719186e13c7b16f6fd38a18473fbc21fc2
Hardik Joshi <joshi.hardik654@gmail.com>
2019-08-13 01:05:01 +0530

    Added sign up and sign in functionality with the help of firebase in the backend (not working properly)

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,49 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+import { AuthService, AuthResponseData } from './auth.service';
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
 export class AuthComponent {
+    isLoginMode = false;
+    isLoading = false;
+    error = null;
 
+    constructor(private authService: AuthService) { }
+
+    onSwitchMode() {
+        this.isLoginMode = !this.isLoginMode;
+    }
+
+    onSubmit(form: NgForm) {
+        if (!form.valid) {
+            return;
+        }
+        const email = form.value.email;
+        const password = form.value.password;
+        this.isLoading = true;
+
+        let authObs: Observable<AuthResponseData>;
+
+        if (this.isLoginMode) {
+            authObs = this.authService.login(email, password);
+        } else {
+            authObs = this.authService.signup(email, password);
+        }
+
+        authObs.subscribe(resData => {
+            console.log(resData);
+            this.isLoading = false;
+        }, errorMessage => {
+            console.log(errorMessage);
+            this.error = errorMessage;
+            this.isLoading = false;
+        });
+
+        form.reset();
+    }
 }
