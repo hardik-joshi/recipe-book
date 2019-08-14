cd4a3089e3bc99d2775ae2419240cb5d2fdb12a7
hardik.joshi <hardik.joshi@eclinicalworks.com>
2019-08-14 09:46:57 -0400

    Added Authentication with the use of interceptor all the request is handled in one place. Now only authorized user can use the application

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -16,36 +16,28 @@ export class DataStorageService {
         this.http
             .put(
                 'https://ng-recipe-book-22f92.firebaseio.com/recipes.json',
-                recipes)
-            .subscribe(response => {
+                recipes
+            ).subscribe(response => {
                 console.log(response);
             });
     }
 
     fetchRecipes() {
-        return this.authService.user
-        .pipe(
-            take(1),
-            exhaustMap(user => {
-                return this.http
-                    .get<Recipe[]>(
-                        'https://ng-recipe-book-22f92.firebaseio.com/recipes.json',
-                        {
-                            params: new HttpParams().set('auth', user.token)
-                        }
-                    );
-            }),
-            map(recipes => {
-                return recipes.map(recipe => {
-                    return {
-                        ...recipe,
-                        ingredients: recipe.ingredients ? recipe.ingredients : []
-                    };
-                });
-            }),
-            tap(recipes => {
-                this.recipeService.setRecipes(recipes);
-            })
-        );
+        return this.http
+            .get<Recipe[]>(
+                'https://ng-recipe-book-22f92.firebaseio.com/recipes.json'
+            ).pipe(
+                map(recipes => {
+                    return recipes.map(recipe => {
+                        return {
+                            ...recipe,
+                            ingredients: recipe.ingredients ? recipe.ingredients : []
+                        };
+                    });
+                }),
+                tap(recipes => {
+                    this.recipeService.setRecipes(recipes);
+                })
+            );
     }
 }
