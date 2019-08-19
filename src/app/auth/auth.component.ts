c163bde608f84819e50120389d45377fe2bca4c7
Hardik Joshi <joshi.hardik654@gmail.com>
2019-08-19 23:23:59 +0530

    Added dynamic component with the help of programming side and not with the help of *ngIf. It is recommended to use *ngIf but this just for learning

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewChild, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { AuthService, AuthResponseData } from './auth.service';
+import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
     isLoginMode = false;
     isLoading = false;
     error = null;
+    @ViewChild(PlaceholderDirective, {static: false}) alertHost: PlaceholderDirective;
+    private closeSub: Subscription;
 
-    constructor(private authService: AuthService, private router: Router) { }
+    constructor(
+        private authService: AuthService, 
+        private router: Router, 
+        private componentFactoryResolver: ComponentFactoryResolver
+    ) { }
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
@@ -43,6 +51,7 @@ export class AuthComponent {
         }, errorMessage => {
             console.log(errorMessage);
             this.error = errorMessage;
+            this.onErrorAlert(errorMessage);
             this.isLoading = false;
         });
 
@@ -51,5 +60,24 @@ export class AuthComponent {
 
     onHandleError() {
         this.error = null;
+    }
+
+    onErrorAlert(message: string) {
+        const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+        const hostViewContainerRef = this.alertHost.viewContainerRef;
+        hostViewContainerRef.clear();
+
+        const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
+        componentRef.instance.message = message;
+        this.closeSub =  componentRef.instance.close.subscribe(() => {
+            this.closeSub.unsubscribe();
+            hostViewContainerRef.clear();
+        });
+    }
+
+    ngOnDestroy() {
+        if(this.closeSub) {
+            this.closeSub.unsubscribe();
+        }
     }
 }
