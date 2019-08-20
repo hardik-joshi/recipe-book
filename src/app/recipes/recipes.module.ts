acb72ba404c328564b1e43b854f20be9961aee92
hardik.joshi <hardik.joshi@eclinicalworks.com>
2019-08-20 05:14:53 -0400

    Added seperate module for recipes and added its own recipes-routing module

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { RecipesComponent } from '../recipes/recipes.component';
+import { RecipeListComponent } from '../recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from '../recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from '../recipes/recipe-detail/recipe-detail.component';
+import { RecipeStartComponent } from '../recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from '../recipes/recipe-edit/recipe-edit.component';
+import { RecipesRoutingModule } from './recipes-routing.module';
+
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeItemComponent,
+        RecipeDetailComponent,
+        RecipeStartComponent,
+        RecipeEditComponent
+    ], 
+    imports: [
+        RouterModule, 
+        CommonModule,
+        ReactiveFormsModule,
+        RecipesRoutingModule
+    ],
+    exports: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeItemComponent,
+        RecipeDetailComponent,
+        RecipeStartComponent,
+        RecipeEditComponent
+    ]
+})
+export class RecipesModule { }
