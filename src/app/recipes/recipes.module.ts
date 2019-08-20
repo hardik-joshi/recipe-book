d047d3031cfa4f1d211624cf12a2d39907b3cb77
hardik.joshi <hardik.joshi@eclinicalworks.com>
2019-08-20 06:28:23 -0400

    Now that we have feature module we can create Shared module which will share the common components with different module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -10,6 +10,7 @@ import { RecipeDetailComponent } from '../recipes/recipe-detail/recipe-detail.co
 import { RecipeStartComponent } from '../recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from '../recipes/recipe-edit/recipe-edit.component';
 import { RecipesRoutingModule } from './recipes-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     declarations: [
@@ -21,10 +22,10 @@ import { RecipesRoutingModule } from './recipes-routing.module';
         RecipeEditComponent
     ], 
     imports: [
-        RouterModule, 
-        CommonModule,
+        RouterModule,
         ReactiveFormsModule,
-        RecipesRoutingModule
+        RecipesRoutingModule,
+        SharedModule
     ]
 })
 export class RecipesModule { }
